d34446e23af4f3640e4ddbe033b6f0462050f181
Iggy-V <ignasvolcokas@gmail.com>
2024-06-04 12:57:38 -0400

    Updated about me page

--- about/about.js
+++ about/about.js
@@ -48,3 +48,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+function showDiv() {
+    var x = document.getElementById('welcomeDiv');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+ }
