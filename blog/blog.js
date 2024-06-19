c4f51c7401773ba7b0a9c1cc3c1faaabf542a224
Iggy-V <ignasvolcokas@gmail.com>
2024-06-19 17:40:45 -0400

    Fixing up blog section

--- blog/blog.js
+++ blog/blog.js
@@ -1,25 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const projects = [
-    //     { id: 1, title: '1st Article', date: '2024-03-02', description: 'My Thoughts on ...', tags: ['Philosophy', 'Movies'], url: 'articles/article-one.html', image: '/assets/esyLearnPortfolio.png'},
-    //     { id: 2, title: '2nd Article', date: '2024-03-02', description: 'My Thoughts on ...', tags: ['Movies'], url: 'projects/esylearn.html', image: '/assets/esyLearnPortfolio.png'},
-    //     { id: 3, title: '3rd Article', date: '2024-03-05', description: 'Exploring the depths of ...', tags: ['Books', 'Philosophy'], url: 'projects/article3.html', image: '/assets/article3.png'},
-    //     { id: 4, title: '4th Article', date: '2024-03-10', description: 'An analysis of ...', tags: ['Politics', 'Travel'], url: 'projects/article4.html', image: '/assets/article4.png'},
-    //     { id: 5, title: '5th Article', date: '2024-03-12', description: 'The evolution of ...', tags: ['Travel', 'Movies'], url: 'projects/article5.html', image: '/assets/article5.png'},
-    //     { id: 6, title: '6th Article', date: '2024-03-15', description: 'A critical review of ...', tags: ['Books', 'Coding'], url: 'projects/article6.html', image: '/assets/article6.png'},
-    //     { id: 7, title: '7th Article', date: '2024-03-20', description: 'The future of ...', tags: ['Other'], url: 'projects/article7.html', image: '/assets/article7.png'},
-    //
+        { id: 1, title: '1st Article', date: '20xx-xx-xx', description: 'My Thoughts on ...', tags: ['Philosophy', 'Movies'], url: '', image: '/assets/work-in-progress.png'},
+        { id: 2, title: '2nd Article', date: '20xx-xx-xx', description: 'My Thoughts on ...', tags: ['Movies', 'Travel'], url: '', image: '/assets/work-in-progress.png'},
+        { id: 3, title: '3rd Article', date: '20xx-xx-xx', description: 'Exploring the depths of ...', tags: ['Books', 'Philosophy'], url: '', image: '/assets/work-in-progress.png'},
+
+    
     ];
 
     const container = document.getElementById('portfolioContainer');
     projects.forEach(project => {
         let tagsHTML = project.tags.map(tag => `<span class="tag ${tag.toLowerCase()}" onclick="event.stopPropagation(); filterProjects('${tag}')">${tag}</span>`).join(' ');
         let imageHTML = project.image ? `<img src="${project.image}" alt="${project.title} image" class="project-image">` : '';
-        let description = project.description.length > 150 ? project.description.substring(0, 147) + '...' : project.description;
-        let projectHTML = `<div class="projectBox ${project.image ? '' : 'no-image'}" data-tags="${project.tags.join(',')}" onclick="window.location.href='${project.url}'">
+        let projectHTML = `<div class="projectBox ${project.image ? '' : 'no-image'}" data-tags="${project.tags.join(',')}"  onclick="window.location.href='${project.url}'">
             ${imageHTML}
             <h3 class="project-title">${project.title}</h3>
             <p class="project-date">${project.date}</p>
-            <p class="project-description">${description} <a href="${project.url}" class="read-more">Read more &#x2192;</a></p>
+            <p class="project-description">${project.description.substring(0, 150)}${project.description.length > 150 ? '...' : ''} <a href="${project.url}" class="read-more">Read more &rarr;</a></p>
             <hr>
             <div class="tags">${tagsHTML}</div><br>
         </div>`;
@@ -28,13 +24,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     const showAllButton = document.getElementById('showAll');
+    const topicTags = document.querySelectorAll('.topic-tag');
 
-    const topicSelect = document.getElementById('topicSelect');
-    topicSelect.addEventListener('change', function() {
-        const selectedTag = this.value.toLowerCase();
-        document.querySelectorAll('.projectBox').forEach(box => {
-            const tags = box.getAttribute('data-tags').toLowerCase();
-            box.style.display = selectedTag === '' || tags.includes(selectedTag) ? '' : 'none';
+    topicTags.forEach(tag => {
+        tag.addEventListener('click', function() {
+            this.classList.toggle('selected');
+            filterProjectsByTags();
         });
     });
 
@@ -42,27 +37,31 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('.projectBox').forEach(box => {
             box.style.display = '';
         });
+        topicTags.forEach(tag => {
+            tag.classList.remove('selected');
+        });
     });
-
 
     const searchInput = document.getElementById('searchInput');
     searchInput.addEventListener('keyup', function() {
-        const searchTerm = this.value.toLowerCase();
+        filterProjectsBySearch();
+    });
+
+    function filterProjectsByTags() {
+        const selectedTags = Array.from(document.querySelectorAll('.topic-tag.selected')).map(tag => tag.getAttribute('data-topic').toLowerCase());
+        document.querySelectorAll('.projectBox').forEach(box => {
+            const tags = box.getAttribute('data-tags').toLowerCase().split(',');
+            const matches = selectedTags.every(tag => tags.includes(tag));
+            box.style.display = matches || selectedTags.length === 0 ? '' : 'none';
+        });
+    }
+
+    function filterProjectsBySearch() {
+        const searchTerm = searchInput.value.toLowerCase();
         document.querySelectorAll('.projectBox').forEach(box => {
             const title = box.querySelector('h3').textContent.toLowerCase();
             const tags = box.querySelector('.tags').textContent.toLowerCase();
             box.style.display = title.includes(searchTerm) || tags.includes(searchTerm) ? '' : 'none';
         });
-    });
-    
+    }
 });
-
-function filterProjects(tag) {
-    const searchTerm = tag.toLowerCase();
-    document.querySelectorAll('.projectBox').forEach(box => {
-        const tags = box.getAttribute('data-tags').toLowerCase();
-        box.style.display = tags.includes(searchTerm) ? '' : 'none';
-    });
-    
-}
-
